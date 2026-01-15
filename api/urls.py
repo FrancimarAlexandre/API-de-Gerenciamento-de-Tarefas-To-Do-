22a02cc76f560fda31afeb6853041d3165d096e6
FrancimarAlexandre <francimaralexandredt@gmail.com>
2026-01-15 07:26:11 -0300

    feat: config serializer, viewset e routers

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,10 @@
-from django.urls import path
+from django.urls import path,include
+from .views import TarefaViewSet
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r"api_all/",TarefaViewSet)
 
 urlpatterns = [
-    
+    path("",include(router.urls)),
 ]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from .serializers import TarefaSerializer
+from .models import Tarefa
+from rest_framework import viewsets
 # Create your views here.
+
+class TarefaViewSet(viewsets.ModelViewSet):
+    queryset = Tarefa.objects.all()
+    serializer_class = TarefaSerializer
