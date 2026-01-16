41fc65b9c7485bfc80d2fba10f53e440e8ab9ed5
FrancimarAlexandre <francimaralexandredt@gmail.com>
2026-01-16 08:43:44 -0300

    fim

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path,include
-from .views import TarefaViewSet
+from .views import TarefaViewset
 from rest_framework import routers
 
 router = routers.DefaultRouter()
-router.register(r"api_all",TarefaViewSet)
-
+router.register(r"ToDoList",TarefaViewset)
 urlpatterns = [
-    path("",include(router.urls)),
+    path('',include(router.urls))
 ]
--- api/views.py
+++ api/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
 from .serializers import TarefaSerializer
 from .models import Tarefa
 from rest_framework import viewsets
-# Create your views here.
 
-class TarefaViewSet(viewsets.ModelViewSet):
+
+# Create your views here.
+class TarefaViewset(viewsets.ModelViewSet):
     queryset = Tarefa.objects.all()
     serializer_class = TarefaSerializer
